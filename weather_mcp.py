a5acdd02c7a9070fdb0f46c64c59f991240c6b0a
cmrfrd <alex@taoa.io>
2025-06-02 17:47:14 +0000

    initial mcp weather

---
+++ weather_mcp.py
@@ -0,0 +1,61 @@
+import httpx
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("weather")
+
+IPINFO_API_BASE = "https://ipinfo.io"
+WEATHER_API_BASE = "https://api.weather.gov"
+
+@mcp.tool()
+async def get_user_location() -> dict[str, str] | None:
+    """Get the coordinates, city, region, and country of the user.
+    """
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(f"{IPINFO_API_BASE}/json")
+            data = response.json()
+        except httpx.HTTPError:
+            return None
+        
+        return {
+            "city": data["city"],
+            "region": data["region"],
+            "country": data["country"],
+            "loc": data["loc"],
+        }
+
+@mcp.tool()
+async def get_forecast(latitude: float, longitude: float) -> str:
+    """Get weather forecast for a location.
+
+    Args:
+        latitude: Latitude of the location
+        longitude: Longitude of the location
+    """
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(f"{WEATHER_API_BASE}/points/{latitude},{longitude}")
+            data = response.json()
+            print(data)
+        except httpx.HTTPError:
+            return "Unable to fetch forecast data for this location."
+        
+        if not data:
+            return "Unable to fetch location data for this location."
+        
+        forecast_url = data["properties"]["forecast"]
+        if not forecast_url:
+            return "No forecast URL found for this location."
+        
+        try:
+            response = await client.get(forecast_url)
+            forecast_data = response.json()
+        except httpx.HTTPError:
+            return "Unable to fetch forecast data for this location."
+
+    forecasts = forecast_data["properties"]["periods"][:3]
+    return "\n---\n".join(str(forecast) for forecast in forecasts)
+
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
